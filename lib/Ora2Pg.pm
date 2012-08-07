darold/ora2pg

commit 01894cbe8d3159e39403edfa6565f676892a7118

Fix case of backslashed single quote when using escape style syntax, they should not be doubled. Report by Stephan Hilb

lib/Ora2Pg.pm
@@ -3946,7 +3946,7 @@ sub format_data_row
 					$row->[$idx] = "E'$row->[$idx]'";
 				}
 			} elsif ($data_type =~ /(char|text|xml)/) {
-				$row->[$idx] =~ s/'/''/gs; # escape single quote
+				$row->[$idx] =~ s/([^\\])'/$1''/gs; # escape single quote
 				if (!$self->{standard_conforming_strings}) {
 					$row->[$idx] =~ s/\\/\\\\/g;
 					$row->[$idx] =~ s/\0//gs;

commit 52d66e23bc80df7fdd4e661acea4621a56ca66d3

Fix escape of single quote at the beginning of string
The wrong behaviour was introduced by an earlier commit (01894cb)
in an attempt to fix escaping of backslashed single quotes.

lib/Ora2Pg.pm
@@ -3947,6 +3947,7 @@ sub format_data_row
 				}
 			} elsif ($data_type =~ /(char|text|xml)/) {
 				$row->[$idx] =~ s/([^\\])'/$1''/gs; # escape single quote
+				$row->[$idx] =~ s/^'/''/gs;
 				if (!$self->{standard_conforming_strings}) {
 					$row->[$idx] =~ s/\\/\\\\/g;
 					$row->[$idx] =~ s/\0//gs;