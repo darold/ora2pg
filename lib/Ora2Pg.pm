0efe9bd4a7926014718ac7498e45b28791f038c9
Darold Gilles <gilles@darold.net>
2013-04-07 18:58:23 +0200

    Update to version 11.1

--- lib/Ora2Pg.pm
+++ lib/Ora2Pg.pm
@@ -38,7 +38,7 @@ use IO::Pipe;
 #set locale to LC_NUMERIC C
 setlocale(LC_NUMERIC,"C");
 
-$VERSION = '11.0';
+$VERSION = '11.1';
 $PSQL = $ENV{PLSQL} || 'psql';
 
 $| = 1;
--- packaging/RPM/ora2pg.spec
+++ packaging/RPM/ora2pg.spec
@@ -4,7 +4,7 @@
 
 Name: %{wname}
 Epoch: 0
-Version: 11.0
+Version: 11.1
 Release: 1%{?dist}
 Summary: Oracle to PostgreSQL database schema converter
 
