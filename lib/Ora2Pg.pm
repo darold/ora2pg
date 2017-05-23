darold/ora2pg

commit 22d2c5adcf56cfcd29aa5e18f4fa66b1b2a537ee

Fix export of package when a comment is placed just before the AS/IS keyword. Thanks to Michael Vitale for the report.

lib/Ora2Pg.pm
@@ -10756,7 +10756,7 @@ sub _convert_package
 	$dirprefix = "$self->{output_dir}/" if ($self->{output_dir});
 	my $content = '';
 
-	if ($self->{package_as_schema} && ($plsql =~ /PACKAGE\s+BODY\s*([^\s]+)\s*(AS|IS)\s*/is)) {
+	if ($self->{package_as_schema} && ($plsql =~ /PACKAGE\s+BODY\s*([^\s]+)(?:\s*\%ORA2PG_COMMENT\d+\%)*\s*(AS|IS)\s*/is)) {
 		my $pname =  $self->quote_object_name($1);
 		$content .= "\nDROP SCHEMA $self->{pg_supports_ifexists} $pname CASCADE;\n";
 		$content .= "CREATE SCHEMA $pname;\n";
@@ -10769,7 +10769,7 @@ sub _convert_package
 	}
 
 	# Convert the package body part
-	if ($plsql =~ /CREATE OR REPLACE PACKAGE\s+BODY\s*([^\s]+)\s*(AS|IS)\s*(.*)/is) {
+	if ($plsql =~ /CREATE OR REPLACE PACKAGE\s+BODY\s*([^\s]+)(?:\s*\%ORA2PG_COMMENT\d+\%)*\s*(AS|IS)\s*(.*)/is) {
 
 		my $pname = $1;
 		my $type = $2;
@@ -10869,7 +10869,7 @@ sub _convert_package
 		}
 
 	# Grab global declaration from the package header
-	} elsif ($plsql =~ /CREATE OR REPLACE PACKAGE\s+([^\s]+)\s*(AS|IS)\s*(.*)/is) {
+	} elsif ($plsql =~ /CREATE OR REPLACE PACKAGE\s+([^\s]+)(?:\s*\%ORA2PG_COMMENT\d+\%)*\s*(AS|IS)\s*(.*)/is) {
 
 		my $pname = $1;
 		my $type = $2;
@@ -14792,7 +14792,7 @@ sub _lookup_package
 
 	my $content = '';
 	my %infos = ();
-	if ($plsql =~ /(?:CREATE|CREATE OR REPLACE)?\s*(?:EDITABLE|NONEDITABLE)?\s*PACKAGE\s+BODY\s*([^\s]+)\s*(AS|IS)\s*(.*)/is) {
+	if ($plsql =~ /(?:CREATE|CREATE OR REPLACE)?\s*(?:EDITABLE|NONEDITABLE)?\s*PACKAGE\s+BODY\s*([^\s]+)((?:\s*\%ORA2PG_COMMENT\d+\%)*\s*AS|IS)\s*(.*)/is) {
 		my $pname = $1;
 		my $type = $2;
 		$content = $3;

commit 72f9c246971c1f729de4a37ae40417bf197ceef8

Fix other cases of comments inside package declaration and a bug introduced by commit 22d2c5a.

lib/Ora2Pg.pm
@@ -4766,7 +4766,7 @@ LANGUAGE plpgsql ;
 			$content =~ s/(?:CREATE|CREATE OR REPLACE)?\s*(?:EDITABLE|NONEDITABLE)?\s*PACKAGE\s+/CREATE OR REPLACE PACKAGE /igs;
 			my @pkg_content = split(/CREATE OR REPLACE PACKAGE BODY\s+/i, $content);
 			for (my $i = 0; $i <= $#pkg_content; $i++) {
-				if ($pkg_content[$i] !~ /^CREATE/) {
+				if ($pkg_content[$i] !~ /^(?:\s*\%ORA2PG_COMMENT\d+\%\s*)?CREATE/is) {
 					if ($pkg_content[$i] =~ /^([^\s]+)/) {
 						my $pname = lc($1);
 						$pname =~ s/"//g;
@@ -14792,7 +14792,7 @@ sub _lookup_package
 
 	my $content = '';
 	my %infos = ();
-	if ($plsql =~ /(?:CREATE|CREATE OR REPLACE)?\s*(?:EDITABLE|NONEDITABLE)?\s*PACKAGE\s+BODY\s*([^\s]+)((?:\s*\%ORA2PG_COMMENT\d+\%)*\s*AS|IS)\s*(.*)/is) {
+	if ($plsql =~ /(?:CREATE|CREATE OR REPLACE)?\s*(?:EDITABLE|NONEDITABLE)?\s*PACKAGE\s+BODY\s*([^\s]+)((?:\s*\%ORA2PG_COMMENT\d+\%)*\s*(?:AS|IS))\s*(.*)/is) {
 		my $pname = $1;
 		my $type = $2;
 		$content = $3;