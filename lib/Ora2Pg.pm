76a8967ae6d7df1e4cc0a44d6ead68d2e434dd74
Darold Gilles <gilles@darold.net>
2017-05-30 11:55:48 +0200

    Apply default oject name exclusion to synonym.

--- lib/Ora2Pg.pm
+++ lib/Ora2Pg.pm
@@ -14823,7 +14823,7 @@ sub limit_to_objects
 		}
 
 		# Always exclude unwanted tables
-		if (!$self->{is_mysql} && !$has_limitation && ($arr_type[$i] =~ /TABLE|SEQUENCE|VIEW|TRIGGER|TYPE/)) {
+		if (!$self->{is_mysql} && !$has_limitation && ($arr_type[$i] =~ /TABLE|SEQUENCE|VIEW|TRIGGER|TYPE|SYNONYME/)) {
 			if ($self->{db_version} =~ /Release [89]/) {
 				$str .= ' AND (';
 				foreach my $t (@EXCLUDED_TABLES_8I) {
