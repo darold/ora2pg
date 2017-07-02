b3101e8d663a046e5aec37c601d57a906044f028
Darold Gilles <gilles@darold.net>
2017-07-02 17:15:39 +0200

    Reapply commit 104cfd0

--- lib/Ora2Pg.pm
+++ lib/Ora2Pg.pm
@@ -2224,6 +2224,9 @@ sub _restore_text_constant_part
 	$$str =~ s/ORA2PG_ESCAPE2_QUOTE/''/gs;
 	$$str =~ s/ORA2PG_ESCAPE1_QUOTE'/\\'/gs;
 
+       if ($self->{type} eq 'TRIGGER') {
+	       $$str =~ s/(\s+)(NEW|OLD)\.'([^']+)'/$1$2\.$3/igs;
+       }
 }
 
 sub _get_dml_from_file
@@ -10938,12 +10941,6 @@ sub data_dump
 		}
 	} else {
 		$self->dump($data);
-	}
-
-	# Rename temporary output file
-	if ($self->{file_per_table} && -e "$dirprefix$filename") {
-		$self->logit("Renaming temporary file $dirprefix$filename into ${dirprefix}${rname}_$self->{output}\n", 1);
-		rename("$dirprefix$filename", "${dirprefix}${rname}_$self->{output}");
 	}
 }
 
