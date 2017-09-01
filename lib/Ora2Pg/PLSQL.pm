3ec2ea52c3fa2e28ef9db8d9e62ef68418b3d1a9
Gilles Darold <gilles@darold.net>
2017-09-01 11:56:29 +0200

    Fix last patch on type replacement in cast() to cover other special case.

--- lib/Ora2Pg/PLSQL.pm
+++ lib/Ora2Pg/PLSQL.pm
@@ -349,6 +349,9 @@ sub convert_plsql_code
 			if ($class->{single_fct_call}{$k} =~ /^CAST\s*\(/i) {
 				$class->{single_fct_call}{$k} = replace_sql_type($class->{single_fct_call}{$k}, $class->{pg_numeric_type}, $class->{default_numeric}, $class->{pg_integer_type}, %data_type);
 			}
+			if ($class->{single_fct_call}{$k} =~ /^CAST\s*\(.*\%\%REPLACEFCT(\d+)\%\%/i) {
+				$class->{single_fct_call}{$1} = replace_sql_type($class->{single_fct_call}{$1}, $class->{pg_numeric_type}, $class->{default_numeric}, $class->{pg_integer_type}, %data_type);
+			}
 		}
 		while ($code_parts[$i] =~ s/\%\%REPLACEFCT(\d+)\%\%/$class->{single_fct_call}{$1}/) {};
 		$code_parts[$i] =~ s/\%SUBPARAMS(\d+)\%/$subparams{$1}/igs;
