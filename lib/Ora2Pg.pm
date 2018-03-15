darold/ora2pg

commit 1dd117c7939e7bc4a864b1a39ff1d901f570aa44

Adapt default value for data type changed to boolean.

lib/Ora2Pg.pm
@@ -6521,8 +6521,12 @@ CREATE TRIGGER ${table}_trigger_insert
 						}
 					} else {
 						if (($f->[4] ne '') && ($self->{type} ne 'FDW')) {
-							if (($type eq 'boolean') && exists $self->{ora_boolean_values}{lc($f->[4])}) {
-								$sql_output .= " DEFAULT '" . $self->{ora_boolean_values}{lc($f->[4])} . "'";
+							if ($type eq 'boolean') {
+								foreach my $k (sort {$b cmp $a} %{ $self->{ora_boolean_values} }) {
+									if ($f->[4] =~ /\b$k\b/i) {
+										$sql_output .= " DEFAULT '" . $self->{ora_boolean_values}{$k} . "'";
+									}
+								}
 							} else {
 								if (($f->[4] !~ /^'/) && ($f->[4] =~ /[^\d\.]/)) {
 									if ($type =~ /CHAR|TEXT|ENUM/i) {

commit 91b2dd6a3ccd7556aef4b0d9eb8115a90ff4864e

Fix bug in boolean default values introduced in commit 1dd117c.

lib/Ora2Pg.pm
@@ -324,6 +324,8 @@ our %BOOLEAN_MAP = (
 	'false' => 'f',
 	'enabled'=> 't',
 	'disabled'=> 'f',
+	't' => 't',
+	'f' => 'f',
 );
 
 our @GRANTS = (
@@ -6519,14 +6521,20 @@ CREATE TRIGGER ${table}_trigger_insert
 						foreach my $c (keys %{$self->{tables}{$table}{column_info}}) {
 							$virtual_trigger_info{$table}{$k} =~ s/\b$c\b/NEW.$c/gs;
 						}
+
 					} else {
+
 						if (($f->[4] ne '') && ($self->{type} ne 'FDW')) {
 							if ($type eq 'boolean') {
+								my $found = 0;
 								foreach my $k (sort {$b cmp $a} %{ $self->{ora_boolean_values} }) {
 									if ($f->[4] =~ /\b$k\b/i) {
 										$sql_output .= " DEFAULT '" . $self->{ora_boolean_values}{$k} . "'";
+										$found = 1;
+										last;
 									}
 								}
+								 $sql_output .= " DEFAULT " . $f->[4] if (!$found);
 							} else {
 								if (($f->[4] !~ /^'/) && ($f->[4] =~ /[^\d\.]/)) {
 									if ($type =~ /CHAR|TEXT|ENUM/i) {