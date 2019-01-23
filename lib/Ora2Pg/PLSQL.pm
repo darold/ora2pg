97afbf3b6be70031346f801b3991ee83ebc6064e
Gilles Darold <gilles.darold@dalibo.com>
2019-01-23 10:20:06 +0100

    Fix translation of CURSOR IS SELECT with a comment before the SELECT. Thanks to Izaak van Niekerk for the report.

--- lib/Ora2Pg/PLSQL.pm
+++ lib/Ora2Pg/PLSQL.pm
@@ -1950,9 +1950,9 @@ sub replace_cursor_def
 	$str =~ s/\bSYS_REFCURSOR\b/REFCURSOR/isg;
 
 	# Replace CURSOR IS SELECT by CURSOR FOR SELECT
-	$str =~ s/\bCURSOR(\s+)IS(\s+)SELECT/CURSOR$1FOR$2SELECT/isg;
+	$str =~ s/\bCURSOR(\s+)IS(\s+)(\%ORA2PG_COMMENT\d+\%)?(\s*)SELECT/CURSOR$1FOR$2$3$4SELECT/isg;
 	# Replace CURSOR (param) IS SELECT by CURSOR FOR SELECT
-	$str =~ s/\bCURSOR(\s*\([^\)]+\)\s*)IS(\s*)SELECT/CURSOR$1FOR$2SELECT/isg;
+	$str =~ s/\bCURSOR(\s*\([^\)]+\)\s*)IS(\s*)(\%ORA2PG_COMMENT\d+\%)?(\s*)SELECT/CURSOR$1FOR$2$3$4SELECT/isg;
 
 	# Replace REF CURSOR as Pg REFCURSOR
 	$str =~ s/\bIS(\s*)REF\s+CURSOR/REFCURSOR/isg;
