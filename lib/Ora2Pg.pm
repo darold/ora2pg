darold/ora2pg

commit d079f2f9fee91c0066e901097bc8e359e294f465

Fix index creation with a CASE clause.

lib/Ora2Pg.pm
@@ -9486,7 +9486,7 @@ sub _create_indexes
 		foreach my $s (@{$indexes{$idx}})
 		{
 			$s = '"' . $s . '"' if ($self->is_reserved_words($s));
-			if ($s =~ /\|\|/) {
+			if ($s =~ /(\|\||CASE\s+.*END)/i) {
 				$columns .= '(' . $s . ')';
 			} else {
 				$columns .= ((exists $opclass_type{$s}) ? $opclass_type{$s} : $s) . ", ";

commit 6b545439aac95ae1dff8a16f6ab9bf28df1254f1

Fix regression introduced by commit d079f2f. Thanks to Sergey Grinko for the report.

lib/Ora2Pg.pm
@@ -7189,7 +7189,7 @@ sub export_table
 			$owner = $self->{force_owner} if ($self->{force_owner} ne "1");
 			$owner ||= $self->{schema};
 			if ($owner && $self->{create_schema}) {
-				$sql_output .= "ALTER SCHEMA " . $self->quote_object_name($self->{pg_schema} || $self->{schema}) . " OWNER TO \L$owner\E;\n";
+				$sql_output .= "ALTER SCHEMA " . $self->quote_object_name($self->{pg_schema} || $self->{schema}) . " OWNER TO " .  $self->quote_object_name($owner) . ";\n";
 			}
 			$sql_output .= "\n";
 		}
@@ -9486,9 +9486,12 @@ sub _create_indexes
 		foreach my $s (@{$indexes{$idx}})
 		{
 			$s = '"' . $s . '"' if ($self->is_reserved_words($s));
-			if ($s =~ /(\|\||CASE\s+.*END)/i) {
+			if ($s =~ /\|\|/i) {
 				$columns .= '(' . $s . ')';
 			} else {
+				if ($s =~ /^CASE\s+.*END/i) {
+					$s = "($s)";
+				}
 				$columns .= ((exists $opclass_type{$s}) ? $opclass_type{$s} : $s) . ", ";
 			}
 			# Add double quotes on column name if PRESERVE_CASE is enabled