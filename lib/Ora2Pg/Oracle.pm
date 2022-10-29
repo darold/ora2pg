b58116c474b0def9f0fa3eb6d31aa948ba7bd2ae
Gilles Darold <gilles@darold.net>
2022-10-29 14:20:30 +0200

    Fix possible "Nested quantifiers in regex" error when exporting package with package name containing regex special characters. Thanks to durandm70 for the report.

--- lib/Ora2Pg/Oracle.pm
+++ lib/Ora2Pg/Oracle.pm
@@ -1723,9 +1723,9 @@ sub _lookup_function
 		next if (!$n || ($pname && (uc($n) !~ /^\U$pname\E\./)));
 		my $tmpname = $n;
 		$tmpname =~ s/^$pname\.//i;
-		next if ($fct_detail{code} !~ /\b$tmpname\b/is);
+		next if ($fct_detail{code} !~ /\b\Q$tmpname\E\b/is);
 		my $i = 0;
-		while ($fct_detail{code} =~ s/(SELECT\s+(?:.*?)\s+)INTO\s+$tmpname\s+([^;]+);/PERFORM set_config('$n', ($1$2), false);/is) { last if ($i++ > 100); };
+		while ($fct_detail{code} =~ s/(SELECT\s+(?:.*?)\s+)INTO\s+\Q$tmpname\E\s+([^;]+);/PERFORM set_config('$n', ($1$2), false);/is) { last if ($i++ > 100); };
 		$i = 0;
 		while ($fct_detail{code} =~ s/\b$n\s*:=\s*([^;]+)\s*;/PERFORM set_config('$n', $1, false);/is) { last if ($i++ > 100); };
 		$i = 0;
