darold/ora2pg

commit 945e9282587311f066a0e07d379333cc5a75e171

Fix issue with multi style comments

lib/Ora2Pg.pm
@@ -14566,6 +14566,15 @@ sub _remove_comments
 		# Single line comment --...-- */ is replaced by  */ only
 		$lines[$i] =~ s/^([\t ]*)\-[\-]+\s*\*\//$1\*\//;
 
+      # Check for -- and */ in the same line
+        if ($lines[$i] =~ /(--.*)(\*\/.*)$/)
+        {
+            my $first_part = $1;
+            my $second_part = $2;
+            $lines[$i] = $first_part;
+            splice(@lines, $i + 1, 0, $second_part);
+        }
+		
 		# Single line comment --
 		if ($lines[$i] =~ s/^([\t ]*\-\-.*)$/$1\%ORA2PG_COMMENT$self->{idxcomment}\%/)
 		{

commit b80b2ad21cfabd4a728119eb361eda994427372e

Fix regression introduced by commit 945e928 on multiline comment

lib/Ora2Pg.pm
@@ -13770,6 +13770,9 @@ sub format_data_type
 			} else {
 				$col =~ s/^([\-]*)(\~|Inf)$/$1Infinity/i;
 			}
+		}
+		else
+		{
 			if (!$sprep) {
 				$col = 'NULL' if ($col eq '');
 			} else {
@@ -14563,16 +14566,19 @@ sub _remove_comments
 	{
 		next if ($lines[$i] !~ /\S/);
 
-		# Single line comment --...-- */ is replaced by  */ only
-		$lines[$i] =~ s/^([\t ]*)\-[\-]+\s*\*\//$1\*\//;
-
-		# Check for -- and */ in the same line
-		if ($lines[$i] =~ /(--.*)(\*\/.*)$/)
+		if ($lines[$i] !~ /^[\t ]*\--.*\/\*.*\*\/.*$/ and $lines[$i] !~ /\/\*.*\*\/$/)
 		{
-			$lines[$i] = $1;
-			splice(@lines, $i + 1, 0, $2);
+			# Single line comment --...-- */ is replaced by  */ only
+			$lines[$i] =~ s/^([\t ]*)\-[\-]+\s*\*\//$1\*\//;
+
+			# Check for -- and */ in the same line
+			if ($lines[$i] =~ /(--.*)(\*\/.*)$/)
+			{
+				$lines[$i] = $1;
+				splice(@lines, $i + 1, 0, $2);
+			}
 		}
-		
+
 		# Single line comment --
 		if ($lines[$i] =~ s/^([\t ]*\-\-.*)$/$1\%ORA2PG_COMMENT$self->{idxcomment}\%/)
 		{